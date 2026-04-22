4a4b39f8e2b09ddc36280105f4e9ab7f6a8c3260
Emilio Latorre <64216678+elatorre20@users.noreply.github.com>
2026-04-22 02:20:46 -0400

    Upload week 5 code

--- week_5.py
+++ week_5.py
@@ -1,5 +1,6 @@
 import turtle
 import math
+import time
 
 class Vector3:
     
@@ -9,22 +10,40 @@ class Vector3:
         self.z = z
         
     def add(self, b):
-        self.x = self.x + b.x
-        self.y = self.y + b.y
-        self.z = self.z + b.z
-        return(self)
-        
+        return Vector3(self.x + b.x, self.y + b.y, self.z + b.z)
+    
+    def __add__(self, b):
+        return self.add(b)
+    
     def sub(self, b):
-        self.x = self.x - b.x
-        self.y = self.y - b.y
-        self.z = self.z - b.z
-        return(self)
-        
+        return Vector3(self.x - b.x, self.y - b.y, self.z - b.z)
+    
+    def __sub__(self, b):
+        return self.subtract(b)
+    
     def mul(self, s):
-        self.x = self.x * s
-        self.y = self.y * s
-        self.z = self.z * s
-        return(self)
+        return Vector3(self.x * s, self.y * s, self.z * s)
+    
+    def __mul__(self, s):
+        return self.mul(s)
+    
+    def cross_product(self, b):
+        c0 = (self.y * b.z) - (self.z * b.y)
+        c1 = (self.z * b.x) - (self.x * b.z)
+        c2 = (self.x * b.y) - (self.y * b.x)
+        return Vector3(c0, c1, c2)
+    
+    def dot_product(self, b):
+        return (self.x * b.x) + (self.y * b.y) + (self.z * b.z)
+    
+    def get_magnitude(self):
+        return math.sqrt(self.x**2 + self.y**2 + self.z**2)
+    
+    def get_normalized(self):
+        magnitude = self.get_magnitude()
+        if magnitude == 0:
+            return Vector3(0, 0, 0)
+        return Vector3(self.x / magnitude, self.y / magnitude, self.z / magnitude)
     
     
 class Polygon:
@@ -79,6 +98,14 @@ class Polygon:
             if(i.z < zmin):
                 zmin = i.z
         return(zmin)
+    
+    def get_normal(self):
+        point0 = self.vertices[0]
+        point1 = self.vertices[1]
+        point2 = self.vertices[2]
+        a = point1.sub(point0)
+        b = point2.sub(point0)
+        return a.cross_product(b).get_normalized()
 
 class Mesh:
     def __init__(self, turtle, polygons=[], offset=Vector3()):
@@ -114,10 +141,16 @@ class Mesh:
         for i in self.polygons:
             i.mirror(a)
     
-    def draw(self):
+    def draw(self, ambient, directional):
         self.polygons = sorted(self.polygons, key = lambda l: l.get_zmin())
         for i in self.polygons:
-            self.turtle.color(i.color)
+            normal = i.get_normal()
+            base = i.color
+            diffuse = max(0, directional.dot_product(normal))
+            light = ambient + diffuse
+            light = min(light, 1)
+            color = (int(base[0] * light),int(base[1] * light),int(base[2] * light))
+            self.turtle.color(color[0], color[1], color[2])
             #draw the face
             self.turtle.penup()
             self.turtle.goto(i.vertices[0].x,i.vertices[0].y)
@@ -128,8 +161,29 @@ class Mesh:
             self.turtle.goto(i.vertices[0].x,i.vertices[0].y)
             self.turtle.end_fill()
             self.turtle.penup()
-#             self.turtle.screen.update()
 
+class Scene:
+    
+    def __init__(self, ambient = 0.05, directional = Vector3(0,0,-1)):
+        self.screen = turtle.Screen()
+        self.screen.colormode(255)
+        self.turtle = turtle.Turtle()
+        self.turtle.hideturtle()
+        self.turtle.speed(0)
+        turtle.tracer(0)
+        self.ambient = ambient
+        self.directional = directional.get_normalized()
+        self.meshes = []
+        
+    def update(self):
+        self.turtle.clear()
+        for i in self.meshes:
+            i.draw(self.ambient, self.directional)
+        turtle.update()
+        
+    def add_cube(self, dim=50, offset=Vector3()):
+        self.meshes.append(make_cube(self.turtle, dim))
+        self.meshes[-1].translate(offset)
 
 def make_triangle(turtle, side = 50, color = "red"):
     height = math.sqrt(3)*side/2
@@ -138,7 +192,14 @@ def make_triangle(turtle, side = 50, color = "red"):
     v2 = Vector3(side/2, -height/3,0)
     return Polygon([v0,v1,v2], color, turtle)
 
-def make_cube(turtle, side=50):
+def make_cube(turtle, side, colors=[
+        (255, 0, 0),      # red
+        (0, 0, 255),      # blue
+        (0, 255, 0),      # green
+        (0, 255, 255),    # cyan
+        (255, 255, 0),    # yellow
+        (255, 0, 255)     # magenta
+    ]):
     h = side / 2
     v = [
         Vector3(-h, -h, -h),  # 0
@@ -151,30 +212,23 @@ def make_cube(turtle, side=50):
         Vector3(-h, h, h)     # 7
     ]
     faces = [
-        Polygon([v[0], v[1], v[2], v[3]], "red", turtle),    # back
-        Polygon([v[4], v[5], v[6], v[7]], "blue", turtle),   # front
-        Polygon([v[0], v[1], v[5], v[4]], "green", turtle),  # bottom
-        Polygon([v[2], v[3], v[7], v[6]], "yellow", turtle), # top
-        Polygon([v[1], v[2], v[6], v[5]], "orange", turtle), # right
-        Polygon([v[0], v[3], v[7], v[4]], "purple", turtle)  # left
+        Polygon([v[0], v[1], v[2], v[3]], colors[0], turtle),    # back
+        Polygon([v[4], v[7], v[6], v[5]], colors[1], turtle),   # front
+        Polygon([v[0], v[4], v[5], v[1]], colors[2], turtle),  # bottom
+        Polygon([v[3], v[2], v[6], v[7]], colors[3], turtle), # top
+        Polygon([v[1], v[5], v[6], v[2]], colors[4], turtle), # right
+        Polygon([v[0], v[3], v[7], v[4]], colors[5], turtle)  # left
     ]
 
     return Mesh(turtle, faces)
 
-t = turtle.Turtle()
-# turtle.tracer(0)
-cube = make_cube(t, 100)
-cube.translate(Vector3(100,0,0))
-cube.rotate(math.pi/4,'y')
-cube.rotate(math.pi/4,'x')
-cube.draw()
-
-cube1 = make_cube(t, 100)
-cube1.translate(Vector3(-100,0,0))
-cube1.rotate(-math.pi/4,'y')
-cube1.rotate(-math.pi/4,'x')
-cube1.draw()
-
-turtle.done()
+scene1 = Scene()
+scene1.add_cube(200)
+scene1.meshes[0].rotate(math.pi/4,"x")
+scene1.meshes[0].rotate(math.pi/4,"z")
+while(True):
+    scene1.meshes[0].rotate(math.pi/64,"y")
+    scene1.update()
+    time.sleep(0.03)
     
     
